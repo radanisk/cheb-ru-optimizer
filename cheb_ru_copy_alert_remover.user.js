1b4557e2ed245fbf7141be7c1b2c0eac0e4af91f
Aleksandr B. Ivanov <radanisk@ya.ru>
2012-11-25 14:48:49 +0400

    Added user script

---
+++ cheb_ru_copy_alert_remover.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name Cheb.Ru CopyAlert Remover
+// @description Cheb.Ru CopyAlert Remover User Script
+// @include http://www.cheb.ru/*
+// @include http://zarulem.ws/*
+// @include http://www.zarulem.ws/*
+// @include http://www.na-svyazi.ru/*
+// ==/UserScript==
+
+document.body.removeAttribute('oncopy');
