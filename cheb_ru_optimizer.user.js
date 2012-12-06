da3863bf762dd594c75763681469698350787f7b
Aleksandr B. Ivanov <radanisk@ya.ru>
2012-12-06 22:27:14 +0400

    При открытии главной страницы cheb.ru автофокус на поисковой строке

--- cheb_ru_optimizer.user.js
+++ cheb_ru_optimizer.user.js
@@ -1,6 +1,8 @@
 // ==UserScript==
 // @name Cheb.Ru Optimizer
+// @namespace https://github.com/Radanisk
 // @description Cheb.Ru Optimizer User Script
+// @version 0.0.2
 // @include http://www.cheb.ru/*
 // @include http://zarulem.ws/*
 // @include http://www.zarulem.ws/*
@@ -8,3 +10,8 @@
 // ==/UserScript==
 
 document.body.removeAttribute('oncopy');
+
+if (/^http:\/\/www\.cheb\.ru\/$/.test(location.href)) {
+    document.getElementById('q').click();
+    document.getElementById('q').focus();
+}
